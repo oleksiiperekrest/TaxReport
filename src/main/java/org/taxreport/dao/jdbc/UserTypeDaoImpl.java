93f05e946f3d5e6d9269116dd488f7048c571020
simurashka <simurashka@gmail.com>
2019-06-01 07:09:49 +0300

    UserTypeDao implemented

--- src/main/java/org/taxreport/dao/jdbc/UserTypeDaoImpl.java
+++ src/main/java/org/taxreport/dao/jdbc/UserTypeDaoImpl.java
@@ -3,20 +3,26 @@ package org.taxreport.dao.jdbc;
 import org.apache.log4j.Logger;
 import org.taxreport.dao.DaoPool;
 import org.taxreport.dao.UserTypeDao;
-import org.taxreport.entity.UserType;
 import org.taxreport.dao.connection.ConnectionPool;
+import org.taxreport.entity.UserType;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class UserTypeDaoImpl implements UserTypeDao {
 
 
-    public static final String SELECT_ID = "SELECT id FROM user_type WHERE status = ?";
+    private static final String SELECT_ID_BY_STATUS = "SELECT id FROM user_type WHERE status = ?";
+    private static final String SELECT_ALL = "SELECT * FROM user_type";
+    private static final String SELECT_BY_ID = "SELECT type FROM user_type WHERE id = ?";
+    private static final String CREATE = "INSERT INTO user_type (status) VALUES (?)";
+    private static final String UPDATE = "UPDATE user_type SET type = ? WHERE (id = ?)";
+    private static final String DELETE = "DELETE FROM user_type WHERE id = ?";
     private final Logger LOGGER = Logger.getLogger(getClass());
 
     private ConnectionPool connectionPool;
@@ -31,7 +37,7 @@ public class UserTypeDaoImpl implements UserTypeDao {
     public Optional<Long> getIdByType(String type) {
         Connection connection = connectionPool.getConnection();
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ID);
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ID_BY_STATUS);
             preparedStatement.setString(1, type);
             ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
@@ -39,8 +45,7 @@ public class UserTypeDaoImpl implements UserTypeDao {
             }
         } catch (SQLException e) {
             LOGGER.error(e);
-        }
-        finally {
+        } finally {
             connectionPool.releaseConnection(connection);
         }
         return Optional.empty();
@@ -49,7 +54,23 @@ public class UserTypeDaoImpl implements UserTypeDao {
 
     @Override
     public void create(UserType userType) {
-
+        Connection connection = connectionPool.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(CREATE);
+            preparedStatement.setString(1, userType.getUserType());
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("Creating user type failed, no rows affected.");
+            }
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                userType.setId(generatedKeys.getLong(1));
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e);
+        } finally {
+            connectionPool.releaseConnection(connection);
+        }
     }
 
     @Override
@@ -72,16 +93,56 @@ public class UserTypeDaoImpl implements UserTypeDao {
 
     @Override
     public List<UserType> getAll() {
-        return null;
+        Connection connection = connectionPool.getConnection();
+        List<UserType> userTypes = new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String status = resultSet.getString("type");
+                userTypes.add(new UserType(id, status));
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e);
+        } finally {
+            connectionPool.releaseConnection(connection);
+        }
+        return userTypes;
     }
 
     @Override
     public void update(UserType userType) {
-
+        Connection connection = connectionPool.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE);
+            preparedStatement.setString(1, userType.getUserType());
+            preparedStatement.setLong(2, userType.getId());
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("Updating user type failed, no rows affected.");
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e);
+        } finally {
+            connectionPool.releaseConnection(connection);
+        }
     }
 
     @Override
     public void delete(UserType userType) {
-
+        Connection connection = connectionPool.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE);
+            preparedStatement.setLong(1, userType.getId());
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("Deleting user type failed, no rows affected.");
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e);
+        } finally {
+            connectionPool.releaseConnection(connection);
+        }
     }
 }
